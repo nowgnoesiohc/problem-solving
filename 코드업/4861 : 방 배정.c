c30da3886af2bd191dab2ee84f23d28add6840a8
nowgnoesiohc <csw6203@naver.com>
2025-01-26 09:14:25 +0900

    Rename 4861 : 방 배정 해결.c to 4861 : 방 배정.c

---
+++ 코드업/4861 : 방 배정.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+  int a[7] = {0}, b[7] = {0};
+  int i, j;
+  int n, m, s, g, tt = 0;
+
+  scanf("%d %d", &n, &m);
+
+  for (i = 0; i < n; i++) {
+    scanf("%d %d", &s, &g);
+    if (s == 0) a[g]++;
+    if (s == 1) b[g]++;
+  }
+
+  for (i = 1; i <= 6; i++) {
+    if (a[i] % m == 0)
+      tt += a[i] / m;
+    else
+      tt += a[i] / m + 1;
+
+    if (b[i] % m == 0)
+      tt += b[i] / m;
+    else
+      tt += b[i] / m + 1;
+  }
+
+  printf("%d", tt);
+  return 0;
+}
