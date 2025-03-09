afa2ef6d643ef4c068254a212c2d74b59739b8ee
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2025-03-09 14:37:32 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 56 ms, Memory: 1384 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,16 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
-    int n, m , i, j, arr[100001]={0}, tmp;
-    scanf("%d %d", &n, &m);
-    for(int k=1;k<=n;k++){
-        scanf("%d", &arr[k]);
-        arr[k] += arr[k-1];
+    int a,b,c,d,n;
+    int arr[100001]={0};
+    scanf("%d %d",&a, &b);
+    for(int i=1;i<=a;i++){
+        scanf("%d",&n);
+        arr[i]=arr[i-1]+n;
     }
-    for(int k=1;k<=m;k++){
-        scanf("%d %d", &i, &j);
-        printf("%d\n", arr[j]-arr[i-1]);
+    for(int i=0;i<b;i++){
+        scanf("%d %d",&c, &d);
+        printf("%d\n",arr[d]-arr[c-1]);
     }
     return 0;
 }
