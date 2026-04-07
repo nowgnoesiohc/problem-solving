a6274030305aaf04429bff9836281dcb6c8e6c1e
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-04-07 16:21:55 +0900

    [level 1] Title: 최소직사각형, Time: 34.02 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,10 +1,10 @@
 function solution(sizes) {
-  let max1 = 0,
-    max2 = 0;
-  for (const size of sizes) {
-    size.sort((a, b) => b - a);
-    if (size[0] > max1) max1 = size[0];
-    if (size[1] > max2) max2 = size[1];
-  }
-  return max1 * max2;
+    let width = 0, height = 0;
+    for (const [w, h] of sizes) {
+        const [left, right] = w > h ? [w, h] : [h, w];
+
+        width = Math.max(width, left);
+        height = Math.max(height, right);
+    }
+    return width * height;
 }
