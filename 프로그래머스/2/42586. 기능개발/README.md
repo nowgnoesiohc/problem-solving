e75d6f03683b10696cb7dd738b38e53a6882f389
Choi Seongwon <77625872+nowgnoesiohc@users.noreply.github.com>
2026-04-08 14:28:02 +0900

    [level 2] Title: 기능개발, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 5.08 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 14일 14:11:13
+2026년 04월 08일 14:28:00
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,26 +1,18 @@
 function solution(progresses, speeds) {
-    var answer = [];
-    const num = progresses.length;
-    let endTime = [];
-    for(let i=0; i<num; i++){
-        endTime.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+    const complete = [];
+    const result = [];
+    for (let i = 0; i < progresses.length; i++) {
+        complete.push(Math.ceil((100 - progresses[i]) / speeds[i]));
     }
-    console.log(endTime)
-
-    let tmp = endTime[0];
-    let cnt = 0;
-    for(let i=1;; i++){
-        if(endTime[0]<=tmp){
-            cnt++;
-            endTime.shift();
-        }
-        else{
-            tmp = endTime[0];
-            answer.push(cnt);
-            cnt = 0;
-        }
-        // console.log(tmp, cnt, answer)
-        if(!endTime.length) {answer.push(cnt); break;}
-    }   
-    return answer;
+    let cnt = 0,
+        tmp = complete[0];
+    for (let i = 0; i < progresses.length; i++) {
+        if (tmp < complete[i]) {
+            result.push(cnt);
+            cnt = 1;
+            tmp = complete[i];
+        } else cnt++;
+    }
+    result.push(cnt);
+    return result;
 }
